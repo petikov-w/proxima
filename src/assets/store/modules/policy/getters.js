d9776090965a734a5434029e05aae55f315ab9f4
krox2 <developer-krox@yandex.ru>
2022-10-04 00:04:04 +0300

    031022-2330 - Project PROXIMA - 04

--- src/assets/store/modules/policy/getters.js
+++ src/assets/store/modules/policy/getters.js
@@ -2,6 +2,10 @@ export const getTitlePage = (state) => state.contentPolicyPage.title_page;
 export const getSubTitlePage = (state) => state.contentPolicyPage.subtitle_page;
 export const getList1Title = (state) => state.contentPolicyPage.page_list1.listTitle;
 export const getList1Items = (state) => state.contentPolicyPage.page_list1.listItems;
+export const getList2Title = (state) => state.contentPolicyPage.page_list2_1.listTitle;
+export const getList2_1_Items = (state) => state.contentPolicyPage.page_list2_1.listItems;
+export const getList2_2_Items = (state) => state.contentPolicyPage.page_list2_2.listItems;
+export const getList2_sub_Items = (state) => state.contentPolicyPage.page_sub_list2.listItems;
 export const getList5Title = (state) => state.contentPolicyPage.page_list5.listTitle;
 export const getList5Items = (state) => state.contentPolicyPage.page_list5.listItems;
 
--- src/pages/PolicyPage.vue
+++ src/pages/PolicyPage.vue
@@ -2,11 +2,22 @@
 .wrapper-home
   .pageTitle {{ pageTitle }}
   .pageText(v-html="pageSubTitle")
-  .pageText.bold(v-html="pageList1Title" )
+  .pageText.bold.b5(v-html="pageList1Title" )
   ul
     li(v-for="item in pageList1Items" :key="index")
       .pageText(v-html="item" )
-  .pageText.bold(v-html="pageList5Title" )
+  .pageText.bold.b5(v-html="pageList2Title" )
+  ul
+    li(v-for="item in pageList2_1_Items" :key="index")
+      .pageText(v-html="item" )
+    ul
+      li(v-for="item in pageList2_sub_Items" :key="index")
+        .pageText(v-html="item" )
+  ul
+    li(v-for="item in pageList2_2_Items" :key="index")
+      .pageText(v-html="item" )
+
+  .pageText.bold.b5(v-html="pageList5Title" )
   ul
     li(v-for="item in pageList5Items" :key="index")
       .pageText(v-html="item" )
@@ -15,8 +26,6 @@
 
 <script>
 
-// import {useRoute} from "vue-router";
-// import store from "@/assets/store/store";
 import {useStore} from 'vuex';
 import {computed} from "vue";
 
@@ -28,6 +37,11 @@ export default {
     const pageSubTitle = computed(() => store.getters.getSubTitlePage);
     const pageList1Title = computed(() => store.getters.getList1Title);
     const pageList1Items = computed(() => store.getters.getList1Items);
+    const pageList2Title = computed(() => store.getters.getList2Title);
+    const pageList2_1_Items = computed(() => store.getters.getList2_1_Items);
+    const pageList2_2_Items = computed(() => store.getters.getList2_2_Items);
+    const pageList2_sub_Items = computed(() => store.getters.getList2_sub_Items);
+
     const pageList5Title = computed(() => store.getters.getList5Title);
     const pageList5Items = computed(() => store.getters.getList5Items);
 
@@ -35,6 +49,10 @@ export default {
              pageSubTitle,
              pageList1Title,
              pageList1Items,
+             pageList2Title,
+             pageList2_1_Items,
+             pageList2_2_Items,
+             pageList2_sub_Items,
              pageList5Title,
              pageList5Items }
   }
@@ -43,6 +61,10 @@ export default {
 
 <style lang="scss" scoped>
   ul{
-    margin-left: 20px;
+    margin-left: 30px;
+  }
+  .b5 {
+    font-size: 20px;
+    opacity: 0.5;
   }
 </style>
