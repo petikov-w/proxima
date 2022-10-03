58310e13bc1a816860b8b656cb3db74b3177154d
Petikov Vladimir <developer-krox@yandex.ru>
2022-10-03 18:28:21 +0300

    031022-1830 Project PROXIMA - 03

--- src/assets/store/modules/policy/getters.js
+++ src/assets/store/modules/policy/getters.js
@@ -1,5 +1,9 @@
 export const getTitlePage = (state) => state.contentPolicyPage.title_page;
 export const getSubTitlePage = (state) => state.contentPolicyPage.subtitle_page;
+export const getList1Title = (state) => state.contentPolicyPage.page_list1.listTitle;
+export const getList1Items = (state) => state.contentPolicyPage.page_list1.listItems;
+export const getList5Title = (state) => state.contentPolicyPage.page_list5.listTitle;
+export const getList5Items = (state) => state.contentPolicyPage.page_list5.listItems;
 
 
 
--- src/pages/PolicyPage.vue
+++ src/pages/PolicyPage.vue
@@ -2,6 +2,14 @@
 .wrapper-home
   .pageTitle {{ pageTitle }}
   .pageText(v-html="pageSubTitle")
+  .pageText.bold(v-html="pageList1Title" )
+  ul
+    li(v-for="item in pageList1Items" :key="index")
+      .pageText(v-html="item" )
+  .pageText.bold(v-html="pageList5Title" )
+  ul
+    li(v-for="item in pageList5Items" :key="index")
+      .pageText(v-html="item" )
 
 </template>
 
@@ -18,13 +26,23 @@ export default {
     const store = useStore();
     const pageTitle = computed(() => store.getters.getTitlePage);
     const pageSubTitle = computed(() => store.getters.getSubTitlePage);
+    const pageList1Title = computed(() => store.getters.getList1Title);
+    const pageList1Items = computed(() => store.getters.getList1Items);
+    const pageList5Title = computed(() => store.getters.getList5Title);
+    const pageList5Items = computed(() => store.getters.getList5Items);
 
     return { pageTitle,
-             pageSubTitle }
+             pageSubTitle,
+             pageList1Title,
+             pageList1Items,
+             pageList5Title,
+             pageList5Items }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+  ul{
+    margin-left: 20px;
+  }
 </style>
